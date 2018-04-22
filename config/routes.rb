5c96f655847fe75faf4955d4c0502ea129973e69
Derek Dyer <derek@digitalderek.com>
2018-04-22 12:24:47 -0400

    Forgot to set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'dashboards#index'
   resources :notify_users, only: %i[create]
   resources :dashboards, only: %i[index]
   resources :subscriptions, only: %i[create]
