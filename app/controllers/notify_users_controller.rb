8d376afcbe5e94b93399bdffee11ddd057b0c998
Derek Dyer <derek@digitalderek.com>
2018-04-25 23:25:07 -0400

    Adds refactoring

--- app/controllers/notify_users_controller.rb
+++ app/controllers/notify_users_controller.rb
@@ -1,17 +1,17 @@
 class NotifyUsersController < ApplicationController
   def create
     Subscription.all.each do |subscription|
-      send_the_message(subscription, params)
+      send_the_message(subscription)
     end
   end
 
   private
 
-  def send_the_message(subscription, params)
+  def send_the_message(subscription)
     Webpush.payload_send(
       endpoint: subscription.endpoint,
       title: params[:title],
-      message: params[:body],
+      message: build_message_object,
       p256dh: subscription.p256dh,
       auth: subscription.auth,
       vapid: {
@@ -22,5 +22,13 @@ class NotifyUsersController < ApplicationController
     )
   rescue Webpush::InvalidSubscription => e
     puts "this is the error: #{e}"
+  end
+
+  def build_message_object
+    {
+      title: params[:title],
+      body: params[:body],
+      icon: 'https://goo.gl/G6Nofu'
+    }.to_json
   end
 end
